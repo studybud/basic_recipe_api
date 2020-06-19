fecf07d4ed7913c7042f024ff7e720dc8cec5835
studybud <study.buddy.0907@gmail.com>
2020-06-19 17:26:25 +0200

    added some more helper test urls

--- helper_urls.py
+++ helper_urls.py
@@ -2,5 +2,9 @@
 urls = [
     'http://127.0.0.1:5000/',
     'http://127.0.0.1:5000/recipes',
-    'http://127.0.0.1:5000/recipes/1'
+    'http://127.0.0.1:5000/recipes/1',
+    'http://127.0.0.1:5000/recipes/1000',
+    'http://127.0.0.1:5000/recipes/name/salad',
+    'http://127.0.0.1:5000/recipes/name/toasted',
+    'http://127.0.0.1:5000/recipes/name/rice'
 ]
